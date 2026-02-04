cefdb910c2596ec1929fd505dc6a10ec096da7d0
mahi180707 <mahiagnihotri07@gmail.com>
2026-02-04 08:29:19 +0530

    Add files via upload

---
+++ day 3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    int arr[100];
+    int i, comparisons = 0;
+    int found = 0;
+
+   
+    scanf("%d", &n);
+
+    
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    scanf("%d", &k);
+
+    
+    for(i = 0; i < n; i++)
+    {
+        comparisons++;
+
+        if(arr[i] == k)
+        {
+            printf("Found at index %d\n", i);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d", comparisons);
+
+    return 0;
+}
