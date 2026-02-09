cbbd009bfd48f983ea3fbeb2f9efad28dfbe4d7c
mahi180707 <mahiagnihotri07@gmail.com>
2026-02-09 09:48:07 +0530

    Add files via upload

---
+++ day8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Recursive function to calculate power
+int power(int a, int b) {
+    if (b == 0)
+        return 1;
+    else
+        return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+
+    // Read input
+    scanf("%d %d", &a, &b);
+
+    // Print result
+    printf("%d", power(a, b));
+
+    return 0;
+}
